88edbcab7c782c3e95d09a595c6fc2c21587c51b
Jean-François GAUTREAU <Jeff59550@gmail.com>
2021-05-24 18:46:45 +0200

    :construction: : start login

--- app/src/main/java/com/goodboi/activities/data/Repository/AuthRepository.kt
+++ app/src/main/java/com/goodboi/activities/data/Repository/AuthRepository.kt
@@ -1,6 +1,21 @@
 package com.goodboi.activities.data.Repository
 
+import androidx.lifecycle.MutableLiveData
 import com.goodboi.activities.data.dataSource.remote.FirebaseAuthService
+import com.google.firebase.auth.FirebaseUser
 
-class AuthRepository(firebaseAuthService: FirebaseAuthService) {
+class AuthRepository(val firebaseAuthService: FirebaseAuthService) {
+
+    fun getCurrentFirebaseUser() = firebaseAuthService.getCurrentFirebaseUser()
+
+    fun signIn(email: String, password: String): MutableLiveData<FirebaseUser?> {
+        val response = MutableLiveData<FirebaseUser?>()
+        firebaseAuthService.signIn(email, password)
+            .addOnSuccessListener {
+                response.value = it.user
+            }.addOnFailureListener() {
+
+            }
+        return response
+    }
 }
--- app/src/main/java/com/goodboi/activities/data/dataSource/remote/FirebaseAuthService.kt
+++ app/src/main/java/com/goodboi/activities/data/dataSource/remote/FirebaseAuthService.kt
@@ -1,5 +1,15 @@
 package com.goodboi.activities.data.dataSource.remote
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+
 class FirebaseAuthService {
 
+    private val auth: FirebaseAuth = Firebase.auth
+
+    fun getCurrentFirebaseUser() = auth.currentUser
+
+    fun signIn(email: String, password: String) = auth.signInWithEmailAndPassword(email, password)
+
+    fun signUp(email: String, password: String) = auth.createUserWithEmailAndPassword(email, password)
 }
