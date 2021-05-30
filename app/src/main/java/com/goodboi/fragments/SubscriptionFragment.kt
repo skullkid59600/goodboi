12e91afbb308eb24e0eb39146f13a167b7434b2c
Jean-François GAUTREAU <Jeff59550@gmail.com>
2021-05-30 18:10:02 +0200

    :sparkles: : signUp Working !

--- app/src/main/java/com/goodboi/fragments/SubscriptionFragment.kt
+++ app/src/main/java/com/goodboi/fragments/SubscriptionFragment.kt
@@ -1,15 +1,20 @@
 package com.goodboi.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.goodboi.R
+import com.goodboi.fragments.viewModel.LoginViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SubscriptionFragment : Fragment() {
+
+    private val loginViewModel: LoginViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,8 +25,17 @@ class SubscriptionFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        view.findViewById<TextView>(R.id.logIn).setOnClickListener {
-            findNavController().navigate(R.id.loginFragment)
+        view.findViewById<TextView>(R.id.signUpButton).setOnClickListener {
+
+            val editTextMail: TextView? = view.findViewById<TextView>(R.id.mailSignUp)
+            val editTextPassword: TextView? = view.findViewById<TextView>(R.id.passwordSignup)
+            // get editText values
+            loginViewModel.signUp(editTextMail?.text.toString(), editTextPassword?.text.toString())
+                .observe(viewLifecycleOwner, Observer {
+                    it?.uid
+                    findNavController().navigate(R.id.loginFragment)
+                })
         }
     }
 }
+
